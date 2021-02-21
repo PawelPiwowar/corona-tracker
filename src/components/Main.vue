b2b7a11297c388509429f2d7bb7672ad0e022454
Paweł Piwowar <pawelpiwowar92@gmail.com>
2021-02-22 00:24:39 +0100

    import-rates change

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -18,7 +18,7 @@
         <div v-if="!isFull">{{ translations[locale].seeAllCountries }} <a :href=" locale==='en' ? '/full' : '/full?lang='+ locale">{{translations[locale].here}}</a>.</div>
         <div v-else> {{ translations[locale].seePolandAndUkraine }} <a :href=" locale==='en' ? '/' : '/?lang='+ locale">{{translations[locale].here}}</a>.</div>
         <div v-for="item in items" v-if="filterData(item)">
-            {{ item.country }}: {{ item.rate_14_day }}
+            <div class="cell">{{ item.country }}: {{ item.rate_14_day }}</div>
         </div>
     </div>
 </template>
@@ -121,3 +121,13 @@ export default {
     },
 }
 </script>
+<style scoped>
+.cell {
+    border: 1px solid black;
+    margin-left: 2px;
+    padding-left: 5px;
+    width: 300px;
+    border-bottom: none;
+}
+
+</style>
